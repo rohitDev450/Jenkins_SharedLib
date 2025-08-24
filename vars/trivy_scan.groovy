6d0f1308f1727b49d6ecc6b147e55501dbd6e7e6
Rohit Aute <157054733+rohitDev450@users.noreply.github.com>
2025-08-24 21:25:22 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,7 +1,7 @@
-def call(){
-  if (!env.TRIVY_PATH) {
-     env.TRIVY_PATH = '/usr/local/bin/trivy'
-  } 
-  env.PATH = "${env.PATH}:${env.TRIVY_PATH}"
-  sh "trivy fs ."
+def call() {
+    if (!env.TRIVY_PATH) {
+        env.TRIVY_PATH = '/usr/local/bin'
+    }
+    env.PATH = "${env.PATH}:${env.TRIVY_PATH}"
+    sh "trivy fs ."
 }
