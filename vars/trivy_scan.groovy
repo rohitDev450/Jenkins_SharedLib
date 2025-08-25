61e7190f6f19859cb8986157477c966d0e62de93
Rohit Aute <157054733+rohitDev450@users.noreply.github.com>
2025-08-25 09:35:40 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,7 +1,16 @@
+// def call() {
+//     if (!env.TRIVY_PATH) {
+//         env.TRIVY_PATH = '/usr/local/bin'
+//     }
+//     env.PATH = "${env.PATH}:${env.TRIVY_PATH}"
+//     sh "trivy fs ."
+// }
 def call() {
-    if (!env.TRIVY_PATH) {
-        env.TRIVY_PATH = '/usr/local/bin'
-    }
-    env.PATH = "${env.PATH}:${env.TRIVY_PATH}"
-    sh "trivy fs ."
+    sh '''
+        docker run --rm \
+          -v /var/run/docker.sock:/var/run/docker.sock \
+          -v $HOME/Library/Caches:/root/.cache/ \
+          -v $(pwd):/project \
+          aquasec/trivy:latest fs /project
+    '''
 }
