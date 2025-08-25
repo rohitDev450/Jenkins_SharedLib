eb5863a4cf73a4b3ddc87f7770461003609664fc
Rohit Aute <157054733+rohitDev450@users.noreply.github.com>
2025-08-25 23:27:37 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,4 @@
 def call() {
-    dependencyCheck additionalArguments: "--scan ./ ${args}", odcInstallation: 'OWASP'
-    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    dependencyCheck additionalArguments: "--scan ./", odcInstallation: 'OWASP'
+    dependencyCheckPublisher pattern: '**/dependency-check-report.*'
 }
