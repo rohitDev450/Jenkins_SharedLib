62978edd106757aaf90b308a9ca9d683eacb2ff6
Rohit Aute <157054733+rohitDev450@users.noreply.github.com>
2025-08-28 12:23:13 +0530

    Update code_checkout.groovy

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,8 +1,22 @@
-def call(String GitUrl, String GitBranch) {
+def call(String GitUrl, String GitBranch, String credsId = '') {
+    def remoteConfig = credsId ? [[url: GitUrl, credentialsId: credsId]] : [[url: GitUrl]]
     checkout([
         $class: 'GitSCM',
-        branches: [[name: GitBranch]],
-        userRemoteConfigs: [[url: GitUrl]],
-        extensions: [[$class: 'WipeWorkspace']]
+        branches: [[name: "*/${GitBranch}"]],
+        userRemoteConfigs: remoteConfig,
+        extensions: []
     ])
 }
+
+
+
+
+
+// def call(String GitUrl, String GitBranch) {
+//     checkout([
+//         $class: 'GitSCM',
+//         branches: [[name: GitBranch]],
+//         userRemoteConfigs: [[url: GitUrl]],
+//         extensions: [[$class: 'WipeWorkspace']]
+//     ])
+// }
